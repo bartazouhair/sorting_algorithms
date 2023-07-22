9faaa3bcf816e0c0e639c745cca171a8f3465a64
bartazouhair <bartazouhair@gmail.com>
2023-07-22 13:07:27 -0700

    100-shell_sort.c Updated

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,36 +1,35 @@
 #include "sort.h"
+
 /**
- * shell_sorty - It's a Sort an array using shell_sort algorithm
- * @array: It's only array
- * @size: It's the size of the array
- * Return: It's NULL because it's void
- **/
+* shell_sort - It's a function sorts an array of integers using
+* the shell sort algorithm with Knuth sequence
+* @array: It's a the array of integers
+* @size: It's a size of the array
+*
+* Return: none because void
+*/
 void shell_sort(int *array, size_t size)
 {
-unsigned int gp = 1, a, b;
+size_t g = 0, y, z;
 int tp;
 
-if (array == NULL)
-return;
 if (size < 2)
 return;
-while (gp < size / 3)
-gp = gp * 3 + 1;
 
-while (gp > 0)
+while ((g = g * 3 + 1) < size)
+;
+
+g = (g - 1) / 3;
+
+for (; g > 0; g = (g - 1) / 3)
 {
-for (a = gp; a < size; a++)
+for (z = g; z < size; i++)
 {
-tp = array[a];
-b = a;
-while (b >= gp && array[b - gp] > tp)
-{
-array[b] = array[b - gp];
-b -= gp;
-}
-array[b] = tp;
+tp = array[z];
+for (y = z; y >= g && tp <= array[y - g]; y -= g)
+array[y] = array[y - g];
+array[y] = tp;
 }
 print_array(array, size);
-gp /= 3;
 }
 }
