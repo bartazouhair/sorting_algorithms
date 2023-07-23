31afb17ebfe865b0bf742c52ad9741e20d963195
bartazouhair <bartazouhair@gmail.com>
2023-07-22 17:15:58 -0700

    File

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,77 +1,84 @@
 #include "sort.h"
 
+void mg(size_t il, size_t md, size_t mh, int *d, int *c);
+void mp(size_t ml, size_t mh, int *ay, int *cp);
 /**
- * merge_sort - It's a sorts an array with the Merge Sort algorithm
- * @array: It's a array of ints to sort
+ * merge_sort - It's A function that sorts an array using merge
+ * @array: It's a The array
  * @size: It's a size of the array
+ * Return: Rien because is void
  */
 void merge_sort(int *array, size_t size)
 {
-int *a;
+size_t w;
+int *p = NULL;
 
 if (!array || size < 2)
 return;
-
-a = malloc(sizeof(int) * size);
-
-MRecursion(a, array, 0, size);
-free(arr);
+p = malloc(sizeof(int) * size);
+if (!p)
+return;
+for (w = 0; w < size; w++)
+p[w] = array[w];
+mp(0, size, array, p);
+free(p);
 }
 
 /**
- * MRecursion - It's a recursive function that merge sorts an array
- * @ar: It's a copy array
- * @arr: It's totaly array to merge
- * @l: It's a index of the left element
- * @r: It's a index of the right element
+ * mg - It's A function that prints subarrays.
+ * @il: It's a index lower
+ * @md: It's a index midlle
+ * @mh: It'a index higher
+ * @d: It's a Data destination
+ * @c: It's a input data
+ * Return: rien because its void
  */
-void MRecursion(int *ar, int *arr, size_t l, size_t r)
+void mg(size_t il, size_t md, size_t mh, int *d, int *c)
 {
-size_t m;
-
-if (r - l > 1)
-{
-m = (r - l) / 2 + l;
-MRecursion(ar, arr, l, m);
-MRecursion(ar, arr, m, r);
-MRecursion(ar, arr, l, m, r);
-}
-}
-
-/**
- * MSubarray - It's a merges subarrays
- * @ar: It's a copy array
- * @arr: It's totaly array to merge
- * @l: It's a index of the left element
- * @m: It's a index of the middle element
- * @r: It's a index of the right element
- */
-void MSubarray(int *ar, int *arr, size_t l, size_t m, size_t r)
-{
-size_t q, g, z = 0;
+size_t z = 0, q = 0, w = 0;
 
 printf("Merging...\n");
 printf("[left]: ");
-print_array(arr + l, m  - l);
+print_array(c + il, md - il);
 printf("[right]: ");
-print_array(arr + m, r - m);
-
-for (q = l, g = m; q < m && g < r; z++)
+print_array(c + md, mh - md);
+z = il;
+q = md;
+w = il;
+for (; w < mh; w++)
 {
-if (arr[q] < arr[q])
-ar[z] = arr[q++];
-else
-ar[z] = arr[g++];
+if (z < md && (q >= mh || c[z] <= c[q]))
+{
+d[w] = c[z];
+z++;
 }
-
-while (q < m)
-ar[z++] = arr[q++];
-while (g < r)
-ar[z++] = arr[g++];
-
-for (z = l, q = 0; z < r; z++)
-arr[z] = ar[q++];
-
+else
+{
+d[w] = c[q];
+q++;
+}
+}
 printf("[Done]: ");
-print_array(arr + l, r - l);
+print_array(d + il, mh - il);
+}
+/**
+ * mp - It's A funtion that separet array
+ * @ml: index loweer
+ * @mh: index higher
+ * @ay: The array
+ * @cp: The copy of arry
+ * Return: Rien parceque it's void
+ */
+void mp(size_t ml, size_t mh, int *ay, int *cp)
+{
+size_t r = 0;
+
+if (mh - ml < 2)
+return;
+r = (ml + mh) / 2;
+mp(ml, r, ay, cp);
+mp(r, mh, ay, cp);
+mg(ml, r, mh, ay, cp);
+for (r = ml; r < mh; r++)
+cp[r] = ay[r];
 }
